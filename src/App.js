433c91f4b5f0260b6f0e4b24e938bfe13485d428
Ali Taha <aliestaha04@gmail.com>
2022-09-17 16:43:35 -0400

    Added functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from "react";
 import Header from "./Header";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import './App.css';
+import Chatbot from "./Chatbot.js";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import { Chat } from "@mui/icons-material";
+
+function HomePage() {
+  return(
+    <div>
+      <h1>I am homepage</h1>
+    </div>
+  )
+}
 
 function App() {
   return(
     <div className="App">
-      <Header />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Header/>} />
+          <Route path='/chat' element={<Chatbot/>} />
+        </Routes>
+      </Router>
+      
     </div>
   )
 }
