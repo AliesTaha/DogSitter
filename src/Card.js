1f36e48504f5649df294b8c5fbe63e33605cd067
Karl Stencell <karldstencell@gmail.com>
2022-09-17 16:42:36 -0400

    added third tile

--- src/Card.js
+++ src/Card.js
@@ -5,10 +5,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Card({name, img}) {
     return ( 
-            <div className='tile' style={{
-                backgroundImage: `url(${img})`,
-                backgroundSize:'cover',
-            }}>
+        <div className='tile' style={{
+            backgroundImage: `url(${img})`,
+            backgroundSize:'cover',
+        }}>
                 <div className='mask d-flex justify-content-start align-items-end'>
                     <div className='card-title'>{name}</div>
                 </div>
--- src/Cards.js
+++ src/Cards.js
@@ -3,6 +3,9 @@ import Card from "./Card";
 // import Card from 'react-bootstrap/Card';
 import './Card.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function Cards() {
     const [people, setPeople] = useState([
@@ -13,13 +16,19 @@ function Cards() {
         {
             name: 'Erlich Bachman',
             url: './assets/erlich.jpg'
+        },
+        {
+            name: 'Jared Dunn',
+            url: './assets/jared.jpg'
         }
     ])
 
     return ( 
         <div>
             {people.map(person => (
-                <Card name={person.name} img={person.url}></Card>
+                <Row className='d-flex justify-content-center p-3'>
+                    <Card name={person.name} img={person.url}></Card>
+                </Row>
             ))}
         </div>
      );
