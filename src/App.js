89ff858b3f6665ea9cf9ec539be6909dc1c70789
Karl Stencell <karldstencell@gmail.com>
2022-09-18 00:48:18 -0400

    updatesssss

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     <div className="App">
       <Header/>
       <Cards/>
-      {/* <Profile/> */}
+      <Profile/>
       {/* <RegistrationPage/> */}
       {/* <Login/> */}
       {/* <Router>
