c67d656f7f7951af6d9988064e7bae7b0360b3f5
Karl Stencell <karldstencell@gmail.com>
2022-09-18 02:57:57 -0400

    fixed card look

--- src/Card.js
+++ src/Card.js
@@ -3,15 +3,20 @@ import './Card.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-function Card({name, img}) {
+function Card({name, bio, img}) {
     return ( 
         <div className='tile' style={{
             backgroundImage: `url(${img})`,
             backgroundSize:'cover',
         }}>
-                <div className='mask d-flex justify-content-start align-items-end'>
-                    <div className='card-title'>{name}</div>
+            <div className='mask d-flex justify-content-start align-items-end'>
+            {/* <div className='mask'> */}
+                {/* <div className='card-title'>{name}</div> */}
+                <div>
+                    <p class="card-title">{name}</p>
+                    <p class="card-text">{bio}</p>
                 </div>
+            </div>
         </div>
      );
 }
--- src/Cards.js
+++ src/Cards.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import Card from "./Card";
 // import Card from 'react-bootstrap/Card';
 import './Card.css'
+import './Cards.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -11,12 +12,12 @@ function Cards() {
     const [people, setPeople] = useState([
         {
             name: 'Richard Hendricks',
-            bio: 'I always knew I was missing something, and then when someone explained the concept of ‘game’ I remember very distinctly thinking ‘That’s what I don’t have.',
+            bio: 'I always knew I was missing.',
             url: './assets/richard.jpg'
         },
         {
             name: 'Erlich Bachman',
-            bio: "Until then, we need to do what any animal in nature does when it's cornered—act erratically and blindly lash out at everything around us.",
+            bio: "Until then, we need to do what",
             url: './assets/erlich.jpg'
         },
         {
@@ -27,13 +28,14 @@ function Cards() {
     ])
 
     return ( 
-        <div>
+        <Row className='p-0 card-group'>
             {people.map(person => (
-                <Row className='d-flex justify-content-center p-3'>
-                    <Card name={person.name} img={person.url}></Card>
-                </Row>
+                // <Row className='d-flex justify-content-center p-3'>
+                    <Col className='col-12 col-lg-6 col-xl-4 d-flex justify-content-center'>
+                        <Card name={person.name} bio={person.bio} img={person.url}></Card>
+                    </Col>
             ))}
-        </div>
+        </Row>
      );
 }
 
